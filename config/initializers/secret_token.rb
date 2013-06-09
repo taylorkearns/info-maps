5a3ff3361b3d5bb718bd747d9e5336924e9b8dc7
Taylor Kearns <tkearns@gmail.com>
2013-06-09 16:57:05 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InfoMaps::Application.config.secret_token = 'ecbe8675cd260d23d4c0405b5715fceb9cabd93189d5944c41134ca57c2b9e4c3f26445c2e344b3802a8b835b0d6275e51bdabaa2113e43488fc837df82188bb'
